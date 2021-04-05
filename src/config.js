cda1442fbd35d6c57e7c1836c93ff1d3d416b4e5
Ming Cheng <cmgrace@bu.edu>
2021-04-05 00:12:09 -0400

    deploy

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-//export const API_ENDPOINT = "https://blooming-cliffs-10607.herokuapp.com/api";
-export const API_ENDPOINT = "http://localhost:8000/api";
+export const API_ENDPOINT = "https://secret-earth-69641.herokuapp.com/api";
+//export const API_ENDPOINT = "http://localhost:8000/api";
 export const TOKEN_KEY = "amazon-client-auth-token";
